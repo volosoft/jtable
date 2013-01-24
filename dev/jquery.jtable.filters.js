fae740a708fe20c0c4eaa9c0ccd18a56b453a9f0
gbisheimer <guillermobisheimer@yahoo.com.ar>
2013-01-24 09:07:31 -0300

    New extensions for JTable
    Filters extension is far from finished, but its a starting point. Also
    included CSS changes for these plugins to work. As Reload and Filters
    adds a command button on table title, I needed to float some things to
    make it possible to stack buttons to the right of title bar.

---
+++ dev/jquery.jtable.filters.js
@@ -0,0 +1,87 @@
+﻿/************************************************************************
+* DATA FILTERING extension for jTable                                  *
+*************************************************************************/
+(function($) {
+
+    //Reference to base object members
+    var base = {
+        _create: $.hik.jtable.prototype._create,
+        _addRowToTable: $.hik.jtable.prototype._addRowToTable,
+        _onRecordsLoaded: $.hik.jtable.prototype._onRecordsLoaded
+    };
+
+    //extension members
+    $.extend(true, $.hik.jtable.prototype, {
+
+        /************************************************************************
+     * DEFAULT OPTIONS / EVENTS                                              *
+     *************************************************************************/
+        options: {
+            showFilterButton: false,
+            messages: {
+                filter: 'Show/hide table filters'
+            }
+        },
+
+        /************************************************************************
+     * PRIVATE FIELDS                                                        *
+     *************************************************************************/
+
+        _$tfilter: null, //Reference to the filter area in top panel
+
+        /************************************************************************
+     * CONSTRUCTOR AND INITIALIZING METHODS                                  *
+     *************************************************************************/
+
+        /* Overrides base method to create footer constructions.
+     *************************************************************************/
+        _create: function() {
+            base._create.apply(this, arguments);
+            if( this.options.showFilterButton ){
+                this._createTableFilter();
+            }
+        },
+        
+        /* Creates footer (all column footers) of the table.
+     *************************************************************************/
+        _createTableFilter: function () {
+            var self = this;
+            
+            var $titleDiv = $('div.jtable-title', this._$mainContainer);
+                
+            self._$tfilter = $('<div class="jtable-filter"></div>')
+            .insertAfter($titleDiv)
+            .hide();
+            $('<button class="jtable-command-button jtable-filter-button" title="' + self.options.messages.filter + '"><span>' + self.options.messages.filter + '</span></button>')
+            .appendTo($titleDiv)
+            .click(function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+                self._$tfilter.slideToggle('fast');
+            });
+            this._addFieldsToTableFilter(this._$tfilter);
+        },
+
+        /* Adds tr element to given tfoot element
+     *************************************************************************/
+        _addFieldsToTableFilter: function ($tfilter) {
+            var self = this;
+            $.each( self.options.fields, function( fieldName, fieldOptions ){
+                if( fieldOptions.filter ){
+                    var $fieldContainer = $('<div class="jtable-input-field-container"></div>').appendTo($tfilter);
+
+                    //Create a label for input
+                    $fieldContainer.append(self._createInputLabelForRecordField(fieldName));
+                    //Create input element with it's current value
+                    $fieldContainer.append(self._createInputForRecordField(fieldName));
+                }
+            });
+        }
+
+    /************************************************************************
+     * OVERRIDED METHODS                                                     *
+     *************************************************************************/
+
+    });
+
+})(jQuery);
---
+++ dev/jquery.jtable.form.template.js
@@ -0,0 +1,117 @@
+﻿/************************************************************************
+* FORM TEMPLATES extension for jTable                                  *
+*************************************************************************/
+(function ($) {
+
+    //Reference to base object members
+    var base = {
+        _create: $.hik.jtable.prototype._create,
+        _showEditForm: $.hik.jtable.prototype._showEditForm
+    };
+
+    //extension members
+    $.extend(true, $.hik.jtable.prototype, {
+
+        /************************************************************************
+     * DEFAULT OPTIONS / EVENTS                                              *
+     *************************************************************************/
+        options: {
+            dialogs: {}
+        },
+
+        /************************************************************************
+     * PRIVATE FIELDS                                                        *
+     *************************************************************************/
+        
+        /************************************************************************
+     * OVERRIDED METHODS                                                     *
+     *************************************************************************/
+        
+        /* Overrides base method to do editing-specific constructions.
+     *************************************************************************/
+        _create: function () {
+            base._create.apply(this, arguments);
+            
+            if( this.options.dialogs.updateDialog != undefined ){
+                if( this.options.dialogs.updateDialog.template != undefined ){
+                    this.options.dialogs.updateDialog.template.remove();    // Removes template from DOM
+                }
+            }
+        },
+
+        /* Shows edit form for a row.
+     *************************************************************************/
+        _showEditForm: function ($tableRow) {
+            var self = this;
+
+            base._showEditForm.apply(this, arguments);
+
+            if( this.options.dialogs.updateDialog != undefined ){
+                var record = $tableRow.data('record');
+                var $editForm = this._$editDiv.find('form#jtable-edit-form');
+                var $dialog = this._$editDiv;
+                var options = this.options.dialogs.updateDialog;
+                
+                this._applyDialogOptions({
+                    dialog: $dialog,
+                    form: $editForm,
+                    formType: 'edit',
+                    options: options,
+                    record: record
+                });
+            }
+        },
+        
+        /************************************************************************
+        * PRIVATE METHODS                                                       *
+        *************************************************************************/        
+
+        /* Apply custom options to forms
+        *************************************************************************/
+        _applyDialogOptions: function ( data ) {
+            var self = this;
+            
+            /*dialog: $dialog,
+            form: $editForm,
+            formType: 'edit',
+            options: options,
+            record: record*/
+            if( data.options.title ){
+                data.dialog.dialog('option', 'title', data.options.title );
+            }
+            
+            if( data.options.template ){
+                //Move created fields to template
+                $.each( data.form.find('div.jtable-input-field-container'), function( index, fieldContainer ){
+                    var $fieldContainer = $(fieldContainer);
+                    var fieldName = $fieldContainer.find('div.jtable-input').children().attr('name');
+                    var $templateFieldDiv = data.options.template.find('div#' + fieldName );
+                
+                    // Checks if field is included in template
+                    if( $templateFieldDiv.length != 0 ){
+                        $templateFieldDiv
+                        .empty()
+                        .append( $fieldContainer );
+                    }
+                });                
+                
+                //Adds template to form
+                data.form.append( data.options.template );
+            }
+            
+            // Applies additional options for created fields
+            $.each( data.form.find('div.jtable-input-field-container'), function( index, fieldContainer ){
+                var $fieldContainer = $(fieldContainer);
+                var $fieldInput = $fieldContainer.find('div.jtable-input').children();
+                var fieldName = $fieldInput.attr('name');
+                var field = self.options.fields[fieldName];
+                
+                // Readonly option
+                if( field.readonly && field.readonly==true ){
+                    $fieldInput
+                    .attr('readonly', field.readonly );
+                }
+            });
+        }
+    });
+})(jQuery);
