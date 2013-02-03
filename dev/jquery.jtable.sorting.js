82df9fff9f0a1ea9b4bc8a91d4481b74f391a36f
Halil İbrahim Kalkan <hikalkan@gmail.com>
2013-02-03 19:48:57 +0200

    added multiSorting option

--- dev/jquery.jtable.sorting.js
+++ dev/jquery.jtable.sorting.js
@@ -19,6 +19,7 @@
         *************************************************************************/
         options: {
             sorting: false,
+            multiSorting: false,
             defaultSorting: ''
         },
 
@@ -31,18 +32,18 @@
         /************************************************************************
         * OVERRIDED METHODS                                                     *
         *************************************************************************/
-        
+
         /* Overrides base method to create sorting array.
         *************************************************************************/
-        _initializeFields: function() {
+        _initializeFields: function () {
             base._initializeFields.apply(this, arguments);
 
             this._lastSorting = [];
-            if( this.options.sorting ){
+            if (this.options.sorting) {
                 this._buildDefaultSortingArray();
             }
-        },        
-        
+        },
+
         /* Overrides _normalizeFieldOptions method to normalize sorting option for fields.
         *************************************************************************/
         _normalizeFieldOptions: function (fieldName, props) {
@@ -75,23 +76,22 @@
 
         /* Builds the sorting array according to defaultSorting string
         *************************************************************************/
-        _buildDefaultSortingArray: function( ) {
+        _buildDefaultSortingArray: function () {
             var self = this;
-            
-            $.each(self.options.defaultSorting.split(","), function( orderIndex, orderValue ){
-                $.each(self.options.fields, function( field, props ){
-                    var colOffset;
-                    if( props.sorting ){
-                        colOffset = orderValue.indexOf(field);
+
+            $.each(self.options.defaultSorting.split(","), function (orderIndex, orderValue) {
+                $.each(self.options.fields, function (fieldName, fieldProps) {
+                    if (fieldProps.sorting) {
+                        var colOffset = orderValue.indexOf(fieldName);
                         if (colOffset > -1) {
-                            if (orderValue.indexOf('DESC', colOffset) > -1) {
+                            if (orderValue.toUpperCase().indexOf('DESC', colOffset) > -1) {
                                 self._lastSorting.push({
-                                    'fieldName': field, 
+                                    fieldName: fieldName,
                                     sortOrder: 'DESC'
                                 });
                             } else {
                                 self._lastSorting.push({
-                                    'fieldName': field, 
+                                    fieldName: fieldName,
                                     sortOrder: 'ASC'
                                 });
                             }
@@ -100,24 +100,32 @@
                 });
             });
         },
-        
+
         /* Makes a column sortable.
         *************************************************************************/
         _makeColumnSortable: function ($columnHeader, fieldName) {
             var self = this;
+            
             $columnHeader
                 .addClass('jtable-column-header-sortable')
-                .click(function(e) {
+                .click(function (e) {
                     e.preventDefault();
-                    if( !e.ctrlKey ) self._lastSorting = [];
+
+                    if (!self.options.multiSorting || !e.ctrlKey) {
+                        self._lastSorting = []; //clear previous sorting
+                    }
+                    
                     self._sortTableByColumn($columnHeader);
                 });
 
-            //Default sorting?
-            $.each(this._lastSorting, function( sortIndex, sortField ){
-                if( sortField.fieldName == fieldName ){
-                    if (sortField.sortOrder == 'DESC') $columnHeader.addClass('jtable-column-header-sorted-desc');
-                    else $columnHeader.addClass('jtable-column-header-sorted-asc');
+            //Set default sorting
+            $.each(this._lastSorting, function (sortIndex, sortField) {
+                if (sortField.fieldName == fieldName) {
+                    if (sortField.sortOrder == 'DESC') {
+                        $columnHeader.addClass('jtable-column-header-sorted-desc');
+                    } else {
+                        $columnHeader.addClass('jtable-column-header-sorted-asc');
+                    }
                 }
             });
         },
@@ -125,16 +133,15 @@
         /* Sorts table according to a column header.
         *************************************************************************/
         _sortTableByColumn: function ($columnHeader) {
-            var self = this;
-            
             //Remove sorting styles from all columns except this one
-            if( this._lastSorting.length == 0 ){
+            if (this._lastSorting.length == 0) {
                 $columnHeader.siblings().removeClass('jtable-column-header-sorted-asc jtable-column-header-sorted-desc');
             }
 
-            for( var i=0; i<self._lastSorting.length; i++ ){
-                if( self._lastSorting[i].fieldName == $columnHeader.data('fieldName') ){
-                    self._lastSorting.splice( i--, 1 );
+            //If current sorting list includes this column, remove it from the list
+            for (var i = 0; i < this._lastSorting.length; i++) {
+                if (this._lastSorting[i].fieldName == $columnHeader.data('fieldName')) {
+                    this._lastSorting.splice(i--, 1);
                 }
             }
 
@@ -142,13 +149,13 @@
             if ($columnHeader.hasClass('jtable-column-header-sorted-asc')) {
                 $columnHeader.removeClass('jtable-column-header-sorted-asc').addClass('jtable-column-header-sorted-desc');
                 this._lastSorting.push({
-                    'fieldName': $columnHeader.data('fieldName'), 
+                    'fieldName': $columnHeader.data('fieldName'),
                     sortOrder: 'DESC'
                 });
             } else {
                 $columnHeader.removeClass('jtable-column-header-sorted-desc').addClass('jtable-column-header-sorted-asc');
                 this._lastSorting.push({
-                    'fieldName': $columnHeader.data('fieldName'), 
+                    'fieldName': $columnHeader.data('fieldName'),
                     sortOrder: 'ASC'
                 });
             }
@@ -165,13 +172,13 @@
             }
 
             var sorting = [];
-            $.each( this._lastSorting, function( idx, value ){
+            $.each(this._lastSorting, function (idx, value) {
                 sorting.push(value.fieldName + ' ' + value.sortOrder);
             });
-            
-            return (url + (url.indexOf('?') < 0 ? '?' : '&') + 'jtSorting=' + sorting.join(",") );
+
+            return (url + (url.indexOf('?') < 0 ? '?' : '&') + 'jtSorting=' + sorting.join(","));
         }
 
     });
-	
+
 })(jQuery);
