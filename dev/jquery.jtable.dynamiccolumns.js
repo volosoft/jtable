ab5873842965932d4985c31e3d1c52c4b645b5c7
Abu Ali Muhammad Sharjeel <sheryever@hotmail.com>
2016-10-09 14:43:48 +0300

    RTL direction support added
    Support added for right to left direction languages like: Arabi, Urdu etc

--- dev/jquery.jtable.dynamiccolumns.js
+++ dev/jquery.jtable.dynamiccolumns.js
@@ -22,7 +22,8 @@
         options: {
             tableId: undefined,
             columnResizable: true,
-            columnSelectable: true
+            columnSelectable: true,
+            rtl: false
         },
 
         /************************************************************************
@@ -268,7 +269,111 @@
         *************************************************************************/
         _makeColumnResizable: function ($columnHeader) {
             var self = this;
+            
+            if (self.options.rtl) {
+                //Create a handler to handle mouse click event
+                $('<div />')
+                    .attr('dir', 'rtl')
+                    .addClass('jtable-column-resize-handler')
+                    .appendTo($columnHeader.find('.jtable-column-header-container')) //Append the handler to the column
+                    .mousedown(function (downevent) { //handle mousedown event for the handler
+                        downevent.preventDefault();
+                        downevent.stopPropagation();
 
+                        var mainContainerOffset = self._$mainContainer.offset();
+
+                        //Get a reference to the next column
+                        var $nextColumnHeader = $columnHeader.nextAll('th.jtable-column-header:visible:first');
+                        if (!$nextColumnHeader.length) {
+                            return;
+                        }
+
+                        //Store some information to be used on resizing
+                        var minimumColumnWidth = 10; //A column's width can not be smaller than 10 pixel.
+                        self._currentResizeArgs = {
+                            currentColumnStartWidth: $columnHeader.outerWidth(),
+                            minWidth: minimumColumnWidth,
+                            maxWidth: $columnHeader.outerWidth() + $nextColumnHeader.outerWidth() - minimumColumnWidth,
+                            mouseStartX: downevent.pageX,
+                            minResizeX: function () {
+                                return this.mouseStartX - (this.currentColumnStartWidth - this.minWidth);
+                            },
+                            maxResizeX: function () {
+                                return this.mouseStartX + (this.maxWidth - this.currentColumnStartWidth);
+                            }
+                        };
+
+                        //Handle mouse move event to move resizing bar
+                        var resizeonmousemove = function (moveevent) {
+                            if (!self._currentResizeArgs) {
+                                return;
+                            }
+
+                            var resizeBarX = self
+                                ._normalizeNumber(moveevent.pageX,
+                                    self._currentResizeArgs.minResizeX(),
+                                    self._currentResizeArgs.maxResizeX());
+                            self._$columnResizeBar.css('left', (resizeBarX - mainContainerOffset.left) + 'px');
+                        };
+
+                        //Handle mouse up event to finish resizing of the column
+                        var resizeonmouseup = function (upevent) {
+                            if (!self._currentResizeArgs) {
+                                return;
+                            }
+
+                            $(document).unbind('mousemove', resizeonmousemove);
+                            $(document).unbind('mouseup', resizeonmouseup);
+
+                            self._$columnResizeBar.hide();
+
+                            //Calculate new widths in pixels
+                            var mouseChangeX = upevent.pageX - self._currentResizeArgs.mouseStartX;
+                            var currentColumnFinalWidth = self
+                                ._normalizeNumber(self._currentResizeArgs.currentColumnStartWidth - mouseChangeX,
+                                    self._currentResizeArgs.minWidth,
+                                    self._currentResizeArgs.maxWidth);
+                            var nextColumnFinalWidth = $nextColumnHeader.outerWidth() +
+                            (self._currentResizeArgs.currentColumnStartWidth - currentColumnFinalWidth);
+
+                            //Calculate widths as percent
+                            var pixelToPercentRatio = $columnHeader.data('width-in-percent') /
+                                self._currentResizeArgs.currentColumnStartWidth;
+                            $columnHeader.data('width-in-percent', currentColumnFinalWidth * pixelToPercentRatio);
+                            $nextColumnHeader.data('width-in-percent', nextColumnFinalWidth * pixelToPercentRatio);
+
+                            //Set new widths to columns (resize!)
+                            $columnHeader.css('width', $columnHeader.data('width-in-percent') + '%');
+                            $nextColumnHeader.css('width', $nextColumnHeader.data('width-in-percent') + '%');
+
+                            //Normalize all column widths
+                            self._normalizeColumnWidths();
+
+                            //Finish resizing
+                            self._currentResizeArgs = null;
+
+                            //Save current preferences
+                            if (self.options.saveUserPreferences) {
+                                self._saveColumnSettings();
+                            }
+                        };
+
+                        //Show vertical resize bar
+                        self._$columnResizeBar
+                            .show()
+                            .css({
+                                top: ($columnHeader.offset().top - mainContainerOffset.top) + 'px',
+                                left: (downevent.pageX - mainContainerOffset.left) + 'px',
+                                height: (self._$table.outerHeight()) + 'px'
+                            });
+
+                        //Bind events
+                        $(document).bind('mousemove', resizeonmousemove);
+                        $(document).bind('mouseup', resizeonmouseup);
+                    });
+                return;
+            } 
+            
             //Create a handler to handle mouse click event
             $('<div />')
                 .addClass('jtable-column-resize-handler')
